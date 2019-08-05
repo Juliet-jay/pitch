ee4a7fc0bb6b812a50785375b925d23f96e18722
kathure <juliekmuthee@gmail.com>
2019-08-05 10:48:56 +0300

    view root page function that returns the index page and its data

--- app/main/views.py
+++ app/main/views.py
@@ -1,2 +1,19 @@
 from flask import render_template
-from app import app
+from . import main
+from ..models import User, Pitch, Category, Vote, Comment
+from flask_login import login_required, current_user
+from .forms import UpdateProfile, PitchForm, CommentForm, CategoryForm
+from .. import db, photos
+
+#Views
+@main.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+   
+    category = Category.get_categories()
+
+
+    return render_template('index.html',  category = category)
